ea67f75dbc82eef6d0510cf77cef1539b2a1dde5
Rakesh Ranjan <rakeshmodi034@gmail.com>
2022-10-10 07:26:29 +0530

    updates

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-const apiUrl = 'https://restcountries.com/v3.1/all';
+const apiUrl = 'https://restcountries.com/v3.1/all/name';
 const searchBtn = document.querySelector('.search-btn');
 searchBtn.addEventListener('click', getCountryData);
 async function getCountryData(){
-    let response = await fetch(apiUrl)
+    let countryName = document.getElementById('myInput').value;
+    let response = await fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
     .then(response => response.json())
     .then(data =>{
         console.log(data);
         let output ='';
-        data.forEach(element => {
-            output += `<li>${element.name.official}</li>`
-        });
+        output += `<li>${data[0].capital}</li>`;
         document.getElementById('result').innerHTML= output;
     })
     .catch(error =>{
